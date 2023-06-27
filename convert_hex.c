1bf780a7f85283686e1939f92c681e76660759b8
Attah247 <122897749+Attah247@users.noreply.github.com>
2023-06-27 02:10:08 +0100

    Update convert_hex.c

--- convert_hex.c
+++ convert_hex.c
@@ -1,6 +1,7 @@
-
 #include "main.h"
-
+/**
+ * Colaborators; Atthah Samuel and Igbashal Grace
+ */
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_X(va_list args, buffer_t *output,
