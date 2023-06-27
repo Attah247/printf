c5722d8a4adfc0d68d9db026e6770d00441ecf08
Attah247 <122897749+Attah247@users.noreply.github.com>
2023-06-27 02:10:31 +0100

    Update convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -1,6 +1,7 @@
-
 #include "main.h"
-
+/**
+ * Colaborators; Atthah Samuel and Igbashal Grace
+ */
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_b(va_list args, buffer_t *output,
