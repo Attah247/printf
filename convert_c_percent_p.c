5f8525638904a96a157c5eb47b12b74a882c7907
Attah247 <122897749+Attah247@users.noreply.github.com>
2023-06-27 02:09:42 +0100

    Update convert_c_percent_p.c

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+/**
+ * Colaborators; Atthah Samuel and Igbashal Grace
+ */
 unsigned int convert_c(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
